d74d149a6c4545defa929dd5bd145871a7215287
Sushant Rao <sushdude34@gmail.com>
2019-12-16 14:09:21 -0500

    Update gpt_2.py

--- gpt_2_simple/gpt_2.py
+++ gpt_2_simple/gpt_2.py
@@ -354,7 +354,7 @@ def finetune(sess,
                         time=time.time() - start_time,
                         loss=v_loss,
                         avg=avg_loss[0] / avg_loss[1],
-                        exp=np.exp(avg_loss[0] / avg_loss[1]))
+                        exp=np.exp(avg_loss[0] / avg_loss[1])))
                 loss_arr.append((avg_loss[0]/avg_loss[1], np.exp(avg_loss[0] / avg_loss[1])))
             counter += 1
     except KeyboardInterrupt:
